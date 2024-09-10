609e255a6fdda7067caa182001a32f6d104e5142
Suvrakamal Das <jyb2084.usa@gmail.com>
2024-09-11 03:34:27 +0530

    Sep 11th

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 import huggingface_hub as hf_hub
 
-source_model = "meta-llama/Meta-Llama-3-8B-Instruct"
+source_model = "subhrokomol/unsloth-mistral7b-en"
 
-target_tokenizer = "hindi_tokenizer"
-export_dir = "new_model"
+target_tokenizer = "subhrokomol/gpt-neo-1.3B-hindi"
+export_dir = "model_folder"
 
 corpus = create_aligned_corpus(
     source_language="en",
@@ -28,12 +28,12 @@ model.save_pretrained(export_dir)
 new_tokenizer.save_pretrained(export_dir)
 
 print(f"finished model creation to {export_dir}")
-# print(f"uploading to HF from {export_dir}")
-# #upload to HF models
-# output_model_name = export_dir
-# repo = hf_hub.create_repo(output_model_name, private=False)  # Set private=False if you want it to be public
-# hf_hub.upload_folder(
-#     folder_path=export_dir,
-#     path_in_repo='',  # Root of the repo
-#     repo_id=f"{hf_hub.whoami()['name']}/{output_model_name}"
-# )
+print(f"uploading to HF from {export_dir}")
+#upload to HF models
+output_model_name = export_dir
+repo = hf_hub.create_repo(output_model_name, private=False)  # Set private=False if you want it to be public
+hf_hub.upload_folder(
+    folder_path=export_dir,
+    path_in_repo='',  # Root of the repo
+    repo_id=f"{hf_hub.whoami()['name']}/{output_model_name}"
+)
